39b8fa72885b31990e29158d1aa2871bda030aaa
Krüger, Bernd <Bernd.Krueger@proalpha.de>
2020-03-19 11:36:55 +0100

    consistency

--- optimizer/message/ml_artort.py
+++ optimizer/message/ml_artort.py
@@ -12,31 +12,31 @@ class ML_ArtOrt(BaseItem):
     """store or place of an article"""
     def __init__(self, tokens, command):
         BaseItem.__init__(self, tokens, command)
-        
+
     def __str__(self):
         text = "bestand: %d, lagerort: %s, mrp_area: %s" % \
             (self.bestand(), self.lagerort(), self.mrp_area())
         return text
-    
+
     def article_id(self):
         """get id"""
         return self._tokens[0]
-    
+
     def bestand(self):
         return self.to_float(self._tokens[1])
-    
+
     def sicherheitsbestand(self):
         return self.to_float(self._tokens[2])
-    
+
     def res_bestand(self):
         return self.to_float(self._tokens[3])
-    
+
     def kommissions_bestand(self):
         return self.to_float(self._tokens[4])
-    
+
     def lagerort(self):
         return self._tokens[5]
-    
+
     def mrp_area(self):
         if len(self._tokens) > 6:
             return self._tokens[6]
@@ -44,10 +44,10 @@ class ML_ArtOrt(BaseItem):
 
     def key_main(self):
         return "%s_%s_%s" % (self.article_id(), self.lagerort(), self.mrp_area())
-    
-    @classmethod        
+
+    @classmethod
     def commands(cls):
         return ['DEF_ERPCommandcreate_ML_Artort____', ]
-    
+
     def token_descriptions(self):
-        return ['ArtID', 'Bestand', 'SBestand', 'Res_b', 'Kom_b', 'LOrt', 'mrp_area', 'reservation']
+        return ['ArtId', 'Bestand', 'SBestand', 'Res_b', 'Kom_b', 'LOrt', 'mrp_area', 'reservation']
--- optimizer/message/ml_artortvar.py
+++ optimizer/message/ml_artortvar.py
@@ -12,32 +12,32 @@ class ML_ArtOrtVar(BaseItem):
     """store or place of an article with a variant"""
     def __init__(self, tokens, command):
         BaseItem.__init__(self, tokens, command)
-        
+
     def __str__(self):
         text = "bestand: %d, ArtVar: %s lagerort: %s, mrp_area: %s" % \
             (self.bestand(), self.artvar(), self.lagerort(), self.mrp_area())
         return text
-    
+
     def article_id(self):
         """get id"""
         return self._tokens[0]
-    
+
     def artvar(self):
         """get artvar"""
         return self._tokens[1]
-    
+
     def bestand(self):
         return self.to_float(self._tokens[2])
-    
+
     def sicherheitsbestand(self):
         return self.to_float(self._tokens[3])
-    
+
     def res_bestand(self):
         return self.to_float(self._tokens[4])
-    
+
     def lagerort(self):
         return self._tokens[5]
-    
+
     def mrp_area(self):
         if len(self._tokens) > 6:
             return self._tokens[6]
@@ -45,11 +45,11 @@ class ML_ArtOrtVar(BaseItem):
 
     def key_main(self):
         return "%s_%s_%s_%s" % (self.article_id(), self.artvar(), self.lagerort(), self.mrp_area)
-    
-    @classmethod        
+
+    @classmethod
     def commands(cls):
         return ['DEF_ERPCommandcreate_ML_Artortvar_', ]
-    
+
     def token_descriptions(self):
-        return ['ArtID', 'part_variant', 'Bestand', 'SBestand', 'Res_b', 'LOrt', 'LGruppe', 
+        return ['ArtId', 'part_variant', 'Bestand', 'SBestand', 'Res_b', 'LOrt', 'LGruppe', 
                 'reservation']
