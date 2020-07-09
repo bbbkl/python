24645929b0e9a9b691b312b1a245066a66af6ab6
Krüger, Bernd <Bernd.Krueger@proalpha.de>
2020-07-09 11:47:54 +0200

    pylint

--- regression/runtime_compare.py
+++ regression/runtime_compare.py
@@ -14,7 +14,6 @@ import os.path
 import csv
 from datetime import datetime
 from argparse import ArgumentParser
-from glob import glob
 
 VERSION = '0.1'
 
@@ -42,20 +41,20 @@ class PerformanceTrace:
         self._job = None
         for line in self._lines:
             self.check_for_job(line)
-    
+
     def line_count(self):
         return len(self._lines)
-    
+
     def add(self, line):
         self._lines.append(line)
         if self._job is None:
             self.check_for_job(line)
-            
+
     def check_for_job(self, line):
         job = get_job(line)
         if job is not None:
             self._job = job
-    
+
     def get_type(self):
         for line in self._lines:
             if line.find('calcCtp')!=-1 or line.find('CTP')!=-1:
@@ -67,19 +66,19 @@ class PerformanceTrace:
                line.find('Optimization finished sync done')!=-1:
                 return 'sync'
         return 'unknown'
-    
+
     def get_mid1(self):
         for line in self._lines:
             if line.find('buildObjectModel')!=-1:
                 return get_datetime(line)
         return None
-    
+
     def get_mid2(self):
         for line in self._lines:
             if line.find('Sent')!=-1:
                 return get_datetime(line)
         return None
-    
+
     def start_to_end(self):
         start = get_datetime(self._lines[0])
         mid1 = self.get_mid1()
@@ -91,16 +90,16 @@ class PerformanceTrace:
         mid = (mid2 - mid1).total_seconds()
         listener = (end - mid2).total_seconds()
         return (int(startup), int(mid), int(listener))
-    
+
     def elapsed_total(self):
         start = get_datetime(self._lines[0])
         end = get_datetime(self._lines[-1])
         diff = end - start
         return diff.total_seconds()
-            
+
     def job(self):
         return self._job
-        
+
     def __str__(self):
         return ''.join(self._lines)
 
@@ -108,7 +107,7 @@ def have_change_job_line(logfile, code):
     for line in open(logfile, encoding=code):
         if re.search(r"^.*Change job number", line):
             return True
-    return False 
+    return False
 
 def show_report(performance_items, stream=sys.stdout):
     timeToItem = {}
@@ -116,25 +115,25 @@ def show_report(performance_items, stream=sys.stdout):
         secs = int(item.elapsed_total())
         timeToItem.setdefault(secs, [])
         timeToItem[secs].append(item)
-    
+
     stream.write('#total: %d\n' % len(performance_items))
     if len(timeToItem) > 0:
         stream.write("\tseconds: #items\n")
     for secs in sorted(timeToItem):
-        stream.write("\t%4d: %4d\n" % (secs, len(timeToItem[secs]))) 
+        stream.write("\t%4d: %4d\n" % (secs, len(timeToItem[secs])))
     print()
-    
-    for idx, item in enumerate(performance_items):  
+
+    for idx, item in enumerate(performance_items):
         line = re.sub(r'[^\x00-\x7F]', '', str(item)) # strip non ascii characters
         stream.write("%02d %s: %s %s\n%s\n" % (idx, item.get_type(), item.elapsed_total(), item.start_to_end(), line))
 
 def test_encoding(message_file):
     """check for file encoding"""
     encodings = ["UTF-8", "ISO-8859-1", "latin-1"]
-    
+
     if not os.path.exists(message_file):
         raise FileNotFoundError(message_file)
-    
+
     for item in encodings:
         try:
             for line in open(message_file, encoding=item):
@@ -142,21 +141,21 @@ def test_encoding(message_file):
             return item
         except:
             pass
-        
+
     raise ("Cannot get right encoding, tried %s" % str(encodings))
 
 def performance_report(logfile, mode_52):
     """logfile and grep performance of single / full optimization"""
-     
+
     code = test_encoding(logfile)
-    
+
     rgx_start = re.compile(r"^.*Change job number")
     if not have_change_job_line(logfile, code):
         rgx_start = re.compile(r"^.*SERVER_STATE\: Receiving Ground Data")
-        
+
     if mode_52:
         rgx_start = re.compile(r"receive Order!")
-    
+
     mid_expressions = [r"SERVER_STATE",
                        r'buildObjectModel Modelltyp',
                        r'-- calcAll --',
@@ -170,23 +169,23 @@ def performance_report(logfile, mode_52):
                        r'elapsed milliseconds since last job start',
                        ]
     rgx_mid = re.compile(r"^.*(%s)" % '|'.join(mid_expressions))
-    
+
     rgx_end = re.compile(r'.*Send CTP\-Prod Solution to ERP.*')
-    
+
     rgx_all = [rgx_start, rgx_mid]
-    
+
     performance_items = []
     job_to_item = {}
     job_to_lines = {}
     last_job_no = None
-    
+
     perf_item = None
     for line in open(logfile, encoding=code):
-        for rgx in rgx_all: 
+        for rgx in rgx_all:
             hit = rgx.search(line)
-            if hit:            
+            if hit:
                 job_no = get_job(line)
-                
+
                 # in 5.2 we have no real start rgx and need to detect end of job
                 if mode_52 and rgx_end.search(line):
                     if perf_item is None:
@@ -196,10 +195,10 @@ def performance_report(logfile, mode_52):
                     performance_items.append(perf_item)
                     perf_item = None
                     break
-                
+
                 if perf_item is None:
                     perf_item = PerformanceTrace(line)
-                    
+
                 elif rgx == rgx_start:
                     if perf_item is not None:
                         if perf_item.line_count()==1 and mode_52:
@@ -232,16 +231,16 @@ def performance_report(logfile, mode_52):
 
     if perf_item is not None:
         performance_items.append(perf_item)
-        
+
     return performance_items
 
 def get_duration(tp1, tp2):
     h1, m1, s1 = [int(x) for x in re.search(r'(\d{2}):(\d{2}):(\d{2})', tp1).groups()]
     h2, m2, s2 = [int(x) for x in re.search(r'(\d{2}):(\d{2}):(\d{2})', tp2).groups()]
-    return h2 * 60 * 60 + m2 * 60 + s2 - (h1 * 60 * 60 + m1 * 60 + s1) 
+    return h2 * 60 * 60 + m2 * 60 + s2 - (h1 * 60 * 60 + m1 * 60 + s1)
   
 def read_times(logfile):
-    times = {}    
+    times = {}
     times.setdefault('ctp', [])
     times.setdefault('opt', "")
     times.setdefault('sync', "")
@@ -249,11 +248,11 @@ def read_times(logfile):
     with open(logfile, "rb") as f:
         first = f.readline() 
 
-        f.seek(-2, os.SEEK_END)        
+        f.seek(-2, os.SEEK_END)
         last = f.readline()
         i = 2
 
-        while get_datetime(last.decode()) is None:            
+        while get_datetime(last.decode()) is None:
             f.seek(-2 * i, os.SEEK_END)
             i = i + 1
             last = f.readline()
@@ -269,7 +268,7 @@ def read_times(logfile):
             times['ctp'].append(perf_item.elapsed_total())
         else:
             print("type: " + perf_item.get_type() + ", job: " + str(perf_item.job()) + ", duration: " + str(perf_item.elapsed_total()))
-    
+
     return times
 
 def compare_logfiles(fn_ref, fn_res):
@@ -312,18 +311,18 @@ def compare_logfiles(fn_ref, fn_res):
 
     line['total_ref'] = ref_times['total']
     line['total_res'] = res_times['total']
-    line['opt_ref'] =  ref_times['opt']    
-    line['opt_res'] =  res_times['opt']    
-    line['sync_ref'] =  ref_times['sync']    
-    line['sync_res'] =  res_times['sync']    
+    line['opt_ref'] =  ref_times['opt']
+    line['opt_res'] =  res_times['opt']
+    line['sync_ref'] =  ref_times['sync']
+    line['sync_res'] =  res_times['sync']
     line['sum_ctp_ref'] = ref_sum_ctp
     line['sum_ctp_res'] = res_sum_ctp
     line['avg_ctp_ref'] = ref_avg_ctp
     line['avg_ctp_res'] = res_avg_ctp
     line['min_ctp_ref'] = ref_min_ctp
-    line['min_ctp_res'] = res_min_ctp    
+    line['min_ctp_res'] = res_min_ctp
     line['max_ctp_ref'] = ref_max_ctp
-    line['max_ctp_res'] = res_max_ctp 
+    line['max_ctp_res'] = res_max_ctp
     line['n_ctp'] = n_ctp
     line['remainder_ref'] = ref_times['total'] - ref_sum_ctp
     if not type(ref_times['opt']) is str:
@@ -341,10 +340,10 @@ def compare_logfiles(fn_ref, fn_res):
         line['avg_ctp_ref'] = ""
         line['avg_ctp_res'] = ""
         line['min_ctp_ref'] = ""
-        line['min_ctp_res'] = ""    
+        line['min_ctp_res'] = ""
         line['max_ctp_ref'] = ""
         line['max_ctp_res'] = ""
-   
+
     return line
 
 def runtime_compare(dir):
@@ -360,7 +359,7 @@ def runtime_compare(dir):
             fn_res = fn_ref.replace('.reference.log', '.result.log')
             fn_head = fn.replace('.reference.log', '')
             print(fn_res)
-            if os.path.exists(fn_res):                
+            if os.path.exists(fn_res):
                 print('comparing logfiles..')
                 line = compare_logfiles(fn_ref, fn_res)
                 line['name'] = fn_head
@@ -368,7 +367,7 @@ def runtime_compare(dir):
 
     #now summarize
     sum = {}
-    sum.setdefault('name', 0) 
+    sum.setdefault('name', 0)
     sum.setdefault('total_ref', 0)
     sum.setdefault('total_res', 0)
     sum.setdefault('opt_ref', 0)
@@ -386,8 +385,8 @@ def runtime_compare(dir):
     sum.setdefault('n_ctp', 0)
     sum.setdefault('remainder_ref', 0)
     sum.setdefault('remainder_res', 0)
-       
-    for (entry) in result:        
+
+    for (entry) in result:
         for (key, value) in entry.items():
             if not type(value) is str:
                 sum[key] += value
@@ -399,7 +398,7 @@ def runtime_compare(dir):
 def write_result_file(result, filename):
     with open(filename, 'w', newline='') as csvfile:
         columns = ['name', 'total_ref', 'total_res', 'opt_ref', 'opt_res', 'sync_ref', 'sync_res', 'n_ctp', 'sum_ctp_ref', 'sum_ctp_res', 'avg_ctp_ref', 'avg_ctp_res', 'min_ctp_ref', 'min_ctp_res', 'max_ctp_ref', 'max_ctp_res', 'remainder_ref', 'remainder_res']
-        writer = csv.DictWriter(csvfile, dialect='excel-tab', fieldnames=columns)        
+        writer = csv.DictWriter(csvfile, dialect='excel-tab', fieldnames=columns)
         writer.writeheader()
         writer.writerows(result)
 
@@ -408,7 +407,7 @@ def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument('-v', '--version', action='version', version=VERSION)
     parser.add_argument('dir', help = 'input directory, compare .reference and .result files')
-	
+
     return parser.parse_args()
 
 def main():
@@ -416,7 +415,7 @@ def main():
     args = parse_arguments()
     dir = args.dir
     result = runtime_compare(dir)
-   	
+
     print(result)
     write_result_file(result, dir + "\\runtime_compare.csv")
 
