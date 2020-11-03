437809cb9c79523299837147615f5f4a44e65367
Krüger, Bernd <Bernd.Krueger@proalpha.de>
2020-11-03 11:37:24 +0100

    improved version

--- id_grabber/split_ctp.py
+++ id_grabber/split_ctp.py
@@ -8,6 +8,7 @@
 
 import os.path
 from argparse import ArgumentParser
+import re
 
 VERSION = '0.1'
 
@@ -29,26 +30,51 @@ def test_encoding(message_file):
 
     raise "Cannot get right encoding, tried %s" % str(encodings)
 
+def get_start_rgx():
+    # 110 = DEF_ERPCommandCheckErpID__________
+    # 250 = DEF_ERPCommandStartJobComplete____ (no contxt)
+    # 111 = DEF_ERPCommandSetConfigParam______
+    return re.compile(r"^(Optimizerversion:|Start\-Config|4\t1\s|2\t110|2\t250|2\t111)")
+
+def get_start_rgx_no_dataline():
+    return re.compile(r"^(Optimizerversion:|Start\-Config|4\t1\s|2\t250)")
+
 def split_ctp(filename):
     """split filename into startup, ctp_001, ctp_002, ..."""
     cnt = 0
     fn_out = filename + '%03d' % cnt
+    cnt += 1
     encoding_id = test_encoding(filename)
     out = open(fn_out, 'w', encoding=encoding_id)
     prev_line = None
+    rgx = get_start_rgx()
+    rgx_no_dataline = get_start_rgx_no_dataline()
+    prev_start_idx = 0
+    idx = -1
     for line in open(filename, encoding=encoding_id):
-        do_separate = (line.find('2\t110') == 0) # 110 DEF_ERPCommandCheckErpID__________
+        idx += 1
+        hit = rgx.search(line)
+        if hit is not None:
+            do_separate = idx - prev_start_idx > 20
+            prev_start_idx = idx
         if do_separate:
-            if cnt > 0:
-                out.close()
-                fn_out = filename + '%03d' % cnt
-                out = open(fn_out, 'w', encoding=encoding_id)
+            do_separate = False
+            #print("SEPARATE idx=%d, line=%s" % (idx, line))
+            if rgx_no_dataline.search(line) is not None and prev_line is not None:
+                out.write("%s" % prev_line)
+                prev_line = None
+            out.close()
+            fn_out = filename + '%03d' % cnt
+            out = open(fn_out, 'w', encoding=encoding_id)
             cnt += 1
+            
         if prev_line is not None:
             out.write("%s" % prev_line)
         prev_line = line
+        
     if prev_line is not None:
         out.write("%s" % prev_line)
+        
     out.close()
 
 def parse_arguments():
